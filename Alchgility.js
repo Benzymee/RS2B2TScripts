62ffc530509310755681ef1cc59f544509eb5c8d
Ben <ben.davis3.14@gmail.com>
2026-09-07 08:42:25 +1000

    WARNING: High Alchs the whole inventory on the Gnome course

--- Alchgility.js
+++ Alchgility.js
@@ -1,6 +1,6 @@
 /**
  * Alchgility. WARNING: High Alchs the whole inventory on the Gnome Stronghold agility course.
- * Load URL: https://cdn.jsdelivr.net/gh/Benzymee/RS2B2TScripts@main/Alchgility.js
+ * Load URL: https://benzymee.github.io/RS2B2TScripts/Alchgility.js
  */
 const SUPPORTED_API_VERSION = 1;
 const abi = globalThis.__rs2b0t;

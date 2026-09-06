e8e9ae61c324d7f16aa0b901f3d0f1083c7c03a7
Ben <ben.davis3.14@gmail.com>
2026-09-07 08:42:26 +1000

    Small-net shrimp at Catherby

--- CatherbyNetFisher.js
+++ CatherbyNetFisher.js
@@ -1,7 +1,7 @@
 /**
  * CatherbyNetFisher. Small-net fishes shrimp at Catherby.
  *
- * Load URL: https://cdn.jsdelivr.net/gh/Benzymee/RS2B2TScripts@main/CatherbyNetFisher.js
+ * Load URL: https://benzymee.github.io/RS2B2TScripts/CatherbyNetFisher.js
  */
 const SUPPORTED_API_VERSION = 1;
 const abi = globalThis.__rs2b0t;

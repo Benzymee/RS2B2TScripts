ac2577d7b7c5e64e79aca72d59db96da80e647d5
Ben <ben.davis3.14@gmail.com>
2026-09-07 08:42:26 +1000

    Craft Body runes from Edgeville bank

--- EdgevilleBodyRunes.js
+++ EdgevilleBodyRunes.js
@@ -1,7 +1,7 @@
 /**
  * EdgevilleBodyRunes. Crafts Body runes from Edgeville bank.
  *
- * Load URL: https://cdn.jsdelivr.net/gh/Benzymee/RS2B2TScripts@main/EdgevilleBodyRunes.js
+ * Load URL: https://benzymee.github.io/RS2B2TScripts/EdgevilleBodyRunes.js
  */
 const SUPPORTED_API_VERSION = 1;
 const abi = globalThis.__rs2b0t;
